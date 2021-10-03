5e1cb3c2a137e6ed8dd95a91068692198398ef11
exoad <exoad@users.noreply.github.com>
2021-10-03 02:32:59 +0000

    Prettified Code!

--- Test.java
+++ Test.java
@@ -1,1 +1,6 @@
-public class Test {public static void main(String[] args) { System.out.println("Tests the prettify workflow lmao");}}/**fdjkfjaldfjadjfksdjfadsfjkdslfjadkslfjsdaklfjdsfdsafas/() */
+public class Test {
+
+  public static void main(String[] args) {
+    System.out.println("Tests the prettify workflow lmao");
+  }
+}/**fdjkfjaldfjadjfksdjfadsfjkdslfjadkslfjsdaklfjdsfdsafas/() */
