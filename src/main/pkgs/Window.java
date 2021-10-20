93b8ca1ccd16be85a5d209c8acd55a87b6321c23
Jack Meng <jackmeng0814@gmail.com>
2021-10-20 14:23:10 -0400

    Update Window.java

--- src/main/pkgs/Window.java
+++ src/main/pkgs/Window.java
@@ -1,5 +1,6 @@
 package main.pkgs;
 
+import java.awt.Dimension;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
@@ -9,7 +10,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Window implements ActionListener, Runnable {
-
+  private JPanel jf;
+  private Dimension windowSize;
+  
+  public Window(String windowName, Dimension windowSize) {
+    this.windowSize = windowSize;
+    jf = new JFrame(windowName);
+  }
+  
   @Override
   public void run() {
 
