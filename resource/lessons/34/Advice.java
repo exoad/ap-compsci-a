28216017dd667b460f305d384c44921960d87c65
Jack Meng <jackmeng0814@gmail.com>
2021-11-09 08:25:44 -0500

    Update Advice.java

--- resource/lessons/34/Advice.java
+++ resource/lessons/34/Advice.java
@@ -11,13 +11,11 @@ public class Advice extends Thought
    //  Prints a message. This method overrides the parent's version.
    //  It also invokes the parent's version explicitly using super.
    //-----------------------------------------------------------------
-//   public void message()
-//   {
-//      System.out.println ("Warning: Dates in calendar are closer " +
-//                          "than they appear.");
-//
-//      System.out.println();
-//
-//      super.message();
-//   }
+     public void message() {
+        System.out.println ("Warning: Dates in calendar are closer " + "than they appear.");
+
+        System.out.println();
+
+        super.message();
+     }
 }
