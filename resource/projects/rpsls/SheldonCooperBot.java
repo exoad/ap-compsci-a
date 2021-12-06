97e78612ba487cdb285a0b60c5befb2975f5d27b
Restyled.io <commits@restyled.io>
2021-12-06 20:29:00 +0000

    Restyled by clang-format

--- resource/projects/rpsls/SheldonCooperBot.java
+++ resource/projects/rpsls/SheldonCooperBot.java
@@ -1,23 +1,13 @@
 package resource.projects.rpsls;
 
 public class SheldonCooperBot implements RPSLSStrategy {
-  public String strategyName() {
-    return "SheldonCooperBot";
-  }
-  
-  public String coderName() {
-    return "Jim Parsons";
-  }
-  
-  public int getThrow() {
-    return 4;
-  }
-  
-  public void opponentsLastThrow(int lastThrow) {
-    
-  }
-  
-  public void reset() {
-    
-  }
+  public String strategyName() { return "SheldonCooperBot"; }
+
+  public String coderName() { return "Jim Parsons"; }
+
+  public int getThrow() { return 4; }
+
+  public void opponentsLastThrow(int lastThrow) {}
+
+  public void reset() {}
 }
