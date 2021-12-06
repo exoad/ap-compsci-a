6046238f89d025fdf0d25bc7e28c1b7a7d6ca18e
Jack Meng <jackmeng0814@gmail.com>
2021-12-06 15:28:47 -0500

    Create SheldonCooperBot.java

---
+++ resource/projects/rpsls/SheldonCooperBot.java
@@ -0,0 +1,23 @@
+package resource.projects.rpsls;
+
+public class SheldonCooperBot implements RPSLSStrategy {
+  public String strategyName() {
+    return "SheldonCooperBot";
+  }
+  
+  public String coderName() {
+    return "Jim Parsons";
+  }
+  
+  public int getThrow() {
+    return 4;
+  }
+  
+  public void opponentsLastThrow(int lastThrow) {
+    
+  }
+  
+  public void reset() {
+    
+  }
+}
