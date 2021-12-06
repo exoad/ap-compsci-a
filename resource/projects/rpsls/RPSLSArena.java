6f1409f053e95333ce2fb83a0b3d6fb4ac3dc7b2
Jack Meng <error666jm@gmail.com>
2021-12-06 15:45:48 -0500

    add

--- resource/projects/rpsls/RPSLSArena.java
+++ resource/projects/rpsls/RPSLSArena.java
@@ -18,17 +18,19 @@ public class RPSLSArena {
 
     JFrame frame = new JFrame("ROCK PAPER SCISSORS LIZARD SPOCK");
     // schedule tournament
-    RPSLSStrategy[] players = {new CopyBot(),          new CycleBot(),
-                               new SheldonCooperBot(), new RockBotTom(),
-                               new Botzilla(),         new OrigamiBot(),
-                               new EdwardScissorBot(), new RandoCalrissian()};
-    RPSLSMatch[] matches =
-        new RPSLSMatch[players.length * (players.length - 1) / 2];
+    /* Replace this
+     * RPSLSStrategy[] players = {new CopyBot(), new CycleBot(),
+     * new SheldonCooperBot(), new RockBotTom(),
+     * new Botzilla(), new OrigamiBot(),
+     * new EdwardScissorBot(), new RandoCalrissian()};
+     */
+    RPSLSStrategy[] players = {};
+    RPSLSMatch[] matches = new RPSLSMatch[players.length * (players.length - 1) / 2];
     for (int p1 = 0, i = 0; p1 < players.length - 1; p1++)
       for (int p2 = p1 + 1; p2 < players.length; p2++, i++)
         matches[i] = new RPSLSMatch(players[p1], players[p2]);
     for (int i = matches.length - 1; i > 0; i--) {
-      int swap = (int)(Math.random() * (i + 1));
+      int swap = (int) (Math.random() * (i + 1));
       RPSLSMatch temp = matches[i];
       matches[i] = matches[swap];
       matches[swap] = temp;
