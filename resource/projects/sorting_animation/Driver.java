5cc06e5093b3c23af2031101ed5cf375d73dd112
Jack Meng <jackmeng0814@gmail.com>
2022-01-03 14:31:58 -0500

    make code

--- resource/projects/sorting_animation/Driver.java
+++ resource/projects/sorting_animation/Driver.java
@@ -1,1 +1,10 @@
-
+public class Driver {
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        new Window().run();
+    }
+}
--- resource/projects/sorting_animation/Window.java
+++ resource/projects/sorting_animation/Window.java
@@ -1,1 +1,51 @@
+import static java.lang.System.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Window {
+    private static abstract class BubbleSort {
+        private BubbleSort() {}
+
+        
+    }
+
+    private static abstract class SelectionSort {
+        private SelectionSort() {}
+
+
+    }
+
+    private static abstract class InsertionSort {
+        private InsertionSort() {}
+
+    }
+
+    private static class GUI implements ActionListener {
+        private static final JButton bubbleSort, selectionSort, insertionSort;
+
+        @Override
+        public boid actionPerformed(ActionEvent e) {
+            
+        }
+    }
+
+    private static class Rectangles {
+        private final int value;
+        public Rectangles(int value) {
+            assert value != null;
+            this.value = value;
+        }
+        public int getValue() {
+            return value;
+        }
+    }
+
+    public Window() {
+
+    }
+}
