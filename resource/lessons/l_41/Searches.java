62827911af53c51a66f1f6a2d03b22432989456d
Jack Meng <jackmeng0814@gmail.com>
2022-01-24 14:25:42 -0500

    Update Searches.java

--- resource/lessons/l_41/Searches.java
+++ resource/lessons/l_41/Searches.java
@@ -3,34 +3,29 @@
 //
 // Demonstrates the linear search and binary search algorithms.
 //********************************************************************
-public class Searches
-{
- public static boolean linearSearch (int[] numbers, int key)
- {
- int index = 0;
- for(int index = 0; index < numbers.length; index++)
- {
- if( key == numbers[index] )
- return true;
- }
- return false;
- }
- public static boolean binarySearch (int[] numbers, int key)
- {
- int low = 0, high = (numbers.length - 1), middle = (low + high)
-/ 2;
- boolean found = false;
- while ( (numbers[middle] != key) && (low <= high) )
- {
- if (key < numbers[middle])
- high = middle - 1;
- else
- low = middle + 1;
- middle = (low + high) / 2
- }
- if (numbers[middle] == key)
- return true;
- else
- return false;
- }
+public class Searches {
+    public static boolean linearSearch(int[] numbers, int key) {
+        int index = 0;
+        for (int index = 0; index < numbers.length; index++) {
+            if (key == numbers[index])
+                return true;
+        }
+        return false;
+    }
+    public static boolean binarySearch(int[] numbers, int key) {
+        int low = 0, high = (numbers.length - 1), middle = (low + high) /
+            2;
+        boolean found = false;
+        while ((numbers[middle] != key) && (low <= high)) {
+            if (key < numbers[middle])
+                high = middle - 1;
+            else
+                low = middle + 1;
+            middle = (low + high) / 2
+        }
+        if (numbers[middle] == key)
+            return true;
+        else
+            return false;
+    }
 }
