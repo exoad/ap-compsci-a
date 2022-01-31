524266ba6f691fe7b823f6694aba9bb3d6719ae6
Jack Meng <jackmeng0814@gmail.com>
2022-01-31 14:11:24 -0500

    update impl

--- resource/projects/maze/Main.java
+++ resource/projects/maze/Main.java
@@ -15,15 +15,25 @@ public class Main {
             this.col = col;
         }
     }
-    private Integer[] ROWC = {0, 1, 0, -1}, COLC = {1, 0, -1, 0};
+    private Integer[] ROWC = {
+        0,
+        1,
+        0,
+        -1
+    }, COLC = {
+        1,
+        0,
+        -1,
+        0
+    };
     private Integer[][] maze;
     private boolean[][] visited;
     private int currRow, currCol;
-    public Main(ArrayList<ArrayList<Integer>> maze) {
-        this.maze = maze.stream().map(u -> u.toArray(new Integer[0])).toArray(Integer[][]::new);
+    public Main(final ArrayList < ArrayList < Integer >> maze) {
+        this.maze = maze.stream().map(u - > u.toArray(new Integer[0])).toArray(Integer[][]::new);
         this.visited = new boolean[maze[0].length][maze.length];
     }
-    
+
     /**
      * <p>
      * This function performs the necessary floodFill operation
@@ -35,25 +45,25 @@ public class Main {
      * @param c Column
      */
     public void flood(int r, int c) {
-        Stack<Point> maps = new Stack<>();
+        Stack < Point > maps = new Stack < > ();
         maps.push(new Point(r, c));
-        while(!maps.isEmpty()) {
+        while (!maps.isEmpty()) {
             Point temp = maps.pop();
             r = temp.row;
             c = temp.col;
-            if(r < 0 || r >= currRow || c < 0 || c >= currCol || Integer[r][c] == 0 || visited[r][c]) 
+            if (r < 0 || r >= currRow || c < 0 || c >= currCol || Integer[r][c] == 0 || visited[r][c])
                 continue;
             visited[r][c] = true;
-            for(int i = 0; i < 4; i++) {
+            for (int i = 0; i < 4; i++) {
                 maps.add(new Point(r + ROWC[i], c + COLC[i]));
             }
         }
     }
-        
+
     public String toString() {
         return null;
     }
-    
+
     public static void main(String[] args) throws java.io.IOException {
         Scanner sc;
         try {
@@ -64,12 +74,12 @@ public class Main {
         }
         sc = new Scanner(System.in);
         System.out.println("File not found, please enter from STDIN.");
-        ArrayList<ArrayList<?>> maze = new ArrayList<>();
-        while(sc.hasNextLine()) {
+        ArrayList < ArrayList < ? >> maze = new ArrayList < > ();
+        while (sc.hasNextLine()) {
             String line = sc.nextLine();
-            maze.add((ArrayList) Arrays.asList(line.toCharArray()));
+            maze.add((ArrayList) Arrays.asList(line.toIntegerArray()));
         }
-        
+
         sc.close();
     }
 }
