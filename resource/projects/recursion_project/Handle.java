a3387706b6a2c6dc5ed68649ed84638a0704445f
Jack Meng <jackmeng0814@gmail.com>
2022-03-03 13:38:00 -0500

    remove

--- resource/projects/recursion_project/Handle.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.JFrame;
-import javax.swing.JButton;
-import java.awt.Dimension;
-import java.awt.Graphics;
-
-public class Handle {
-
-}
--- resource/projects/recursion_project/Runner.java
+++
@@ -1,8 +0,0 @@
-public class Runner {
-  public static void main(String... args) {
-    ClientClass exit = new ClientClass("Do you want to exit? (y/n):");
-    do {
-    } while(!exit.getOutput().equals("y") || !exit.getOutput().equals("Y"));
-
-  }
-}
