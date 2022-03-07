95c8c96f82cf93941d48d126c38db616a2102ca6
Jack Meng <jackmeng0814@gmail.com>
2022-03-07 13:47:10 -0500

    reread

--- resource/projects/recursion_project/Solver.java
+++ resource/projects/recursion_project/Solver.java
@@ -3,13 +3,14 @@ import java.awt.Dimension;
 public class Solver {
     private Dimension limit;
     private ArrayList<Pair<Pair<Double, Double>, Pair<Double, Double>>> lines;
-    public Solver(Dimension screenSize) {
+    private levels = 1;
+    public Solver(Dimension screenSize, int levels) {
         this.limit = screenSize;
+        this.levels = levels;
     }
 
     private ArrayList<Pair<Pair<Double, Double>, Pair<Double, Double>>> enFill(ArrayList<Pair<Pair<?, ?>, Pair<?, ?>>> def) {
-        def.forEach(() -> {
-
-        });
+        for(int i = 0; i < levels; i++) {
+        }
     }
 }
