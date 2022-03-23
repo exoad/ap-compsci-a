c244680504742da0d42a2cecded51cf1b9f33cab
Jack Meng <jackmeng0814@gmail.com>
2022-03-23 13:42:13 -0400

    Update Config.java

--- resource/projects/animated_game/Config.java
+++ resource/projects/animated_game/Config.java
@@ -16,8 +16,8 @@ public class Config {
   public static final Color TOOLTIP_BG = new Color(224, 108, 118);
   public static final Color TOOLTIP_FG = new Color(10, 12, 16);
   public static final Color DEBUG_NEIGHBOR = new Color(255, 41, 251);
-  public static final long MAX_DELAY = 300L;
-  public static final long REFRESH_GENERATION_TIME_MS = 200L;
+  public static final long MAX_TLE = 300L;
+  public static final long MAX_GENERATION_TLE = 200L;
   public static final int GRID_MODIFIER = 85;
   public static final int VERT_STRUCT = 10;
   public static final String GAME_MANUAL_URL = "http://pi.math.cornell.edu/~lipa/mec/lesson6.html"; 
